170869b0376f1726905e9b7dae0f06ba80a7c018
rinnan17 <magnus.rinnan@nav.no>
2022-03-25 13:27:47 +0100

    Oppdatert til ny struktur

---
+++ src/main/kotlin/no/nav/bidrag/behandling/felles/dto/stonad/mottakeridhistorikk.kt
@@ -0,0 +1,27 @@
+package no.nav.bidrag.behandling.felles.dto.stonad
+
+import io.swagger.v3.oas.annotations.media.Schema
+import java.time.LocalDateTime
+
+interface MottakerIdHistorikkService {
+  fun finnAlleEndringerMottakerIdForStonad(stonadId: Int): StonadDto
+}
+
+data class MottakerIdHistorikkDto(
+
+  @Schema(description = "stonad-id")
+  val stonadId: Int = 0,
+
+  @Schema(description = "Utgått Mottaker-Id")
+  val mottakerIdEndretFra: String = "",
+
+  @Schema(description = "Ny Mottaker-Id")
+  val mottakerIdEndretTil: String = "",
+
+  @Schema(description = "Saksbehandler/batchjobb/annet som har oppdatert mottaker-id")
+  val opprettetAv: String = "",
+
+  @Schema(description = "Opprettet timestamp")
+  val opprettetTimestamp: LocalDateTime = LocalDateTime.now()
+)
+
--- src/main/kotlin/no/nav/bidrag/behandling/felles/dto/stonad/periode.kt
+++ src/main/kotlin/no/nav/bidrag/behandling/felles/dto/stonad/periode.kt
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema
 import java.math.BigDecimal
 import java.time.LocalDate
 
-data class HentPeriodeResponse(
+data class PeriodeDto(
 
   @Schema(description = "Periode-id")
   val periodeId: Int = 0,
