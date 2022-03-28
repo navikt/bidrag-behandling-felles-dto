404562a5db8d71abe3b18a4a844df2b659996349
rinnan17 <magnus.rinnan@nav.no>
2022-03-28 11:21:21 +0200

    Endret til kun ett interface

--- src/main/kotlin/no/nav/bidrag/behandling/felles/dto/stonad/client.kt
+++ src/main/kotlin/no/nav/bidrag/behandling/felles/dto/stonad/client.kt
@@ -1,12 +1,10 @@
 package no.nav.bidrag.behandling.felles.dto.stonad
 
-interface MottakerIdHistorikkClient {
-  fun finnAlleEndringerMottakerIdForStonad(stonadId: Int): HentStonadDto
-}
-
-
 interface StonadClient {
   fun hentStonad(stonadId: Int): HentStonadDto
 
   fun endreMottakerIdOgOpprettHistorikk(request: EndreMottakerIdRequestDto): MottakerIdHistorikkDto
+
+  fun finnAlleEndringerMottakerIdForStonad(stonadId: Int): HentStonadDto
+
 }
