715cc995fa01bbebad96f72a81c8fec328df68ad
rinnan17 <magnus.rinnan@nav.no>
2022-05-06 14:29:12 +0200

    Lagt til dto'er for pdl-data fra bidrag-grunnlag

--- src/main/kotlin/no/nav/bidrag/behandling/felles/dto/grunnlag/response.kt
+++ src/main/kotlin/no/nav/bidrag/behandling/felles/dto/grunnlag/response.kt
@@ -3,6 +3,7 @@ package no.nav.bidrag.behandling.felles.dto.grunnlag
 import io.swagger.v3.oas.annotations.media.Schema
 import no.nav.bidrag.behandling.felles.enums.GrunnlagRequestType
 import no.nav.bidrag.behandling.felles.enums.GrunnlagsRequestStatus
+import no.nav.bidrag.behandling.felles.enums.SivilstandKode
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -51,7 +52,10 @@ data class HentGrunnlagspakkeDto(
   val ubstListe: List<UtvidetBarnetrygdOgSmaabarnstilleggDto>,
 
   @Schema(description = "Liste over innhentet barnetillegg")
-  val barnetilleggListe: List<BarnetilleggDto>
+  val barnetilleggListe: List<BarnetilleggDto>,
+
+  @Schema(description = "Liste over innhentet barnetillegg")
+  val husstandsmedlemmerListe: List<Husstand>
 )
 
 
@@ -229,4 +233,132 @@ data class BarnetilleggDto(
 
   @Schema(description = "Hentet tidspunkt")
   val hentetTidspunkt: LocalDateTime
+)
+
+data class VoksneHusstandsmedlemmerDto(
+  @Schema(description = "Gir periodisert informasjon om hvilke husstander BP har bodd i/bor i, og en periodisert liste over de som bor/bodde der")
+  val husstandListe: List<Husstand>?
+)
+
+data class Husstand(
+  @Schema(description = "Personen (BP) bor i husstanden fra- og med måned")
+  val periodeFra: LocalDate,
+
+  @Schema(description = "Personen (BP) bor i husstanden til- og med måned")
+  val periodeTil: LocalDate?,
+
+  @Schema(description = "Navnet på en gate e.l.")
+  val adressenavn: String?,
+
+  @Schema(description = "Nummeret som identifiserer et av flere hus i en gate")
+  val husnummer: String?,
+
+  @Schema(description = "Bokstav som identifiserer del av et bygg")
+  val husbokstav: String?,
+
+  @Schema(description = "En bokstav og fire siffer som identifiserer en boligenhet innenfor et bygg eller en bygningsdel")
+  val bruksenhetsnummer: String?,
+
+  @Schema(description = "Norsk postnummer")
+  val postnr: String?,
+
+  @Schema(description = "6 siffer, identifiserer bydel")
+  val bydelsnummer: String?,
+
+  @Schema(description = "Siffer som identifiserer hvilken kommune adressen ligger i")
+  val kommunenummer: String?,
+
+  @Schema(description = "Nøkkel til geografisk adresse registrert i Kartverkets matrikkel")
+  val matrikkelId: String?,
+
+  @Schema(description = "Manuelt opprettet av")
+  val opprettetAv: String?,
+
+  @Schema(description = "Lagret tidspunkt")
+  val lagretTidspunkt: LocalDateTime,
+
+  @Schema(description = "Periodisert liste over husstandsmedlemmer")
+  val husstandsmedlemmerListe: List<Husstandsmedlem>?
+)
+
+data class Husstandsmedlem(
+  @Schema(description = "Identen til husstandsmedlemmet")
+  var personId: String?,
+
+  @Schema(description = "Navn på husstandsmedlemmet, format <Fornavn, mellomnavn, Etternavn")
+  var navn: String?,
+
+  @Schema(description = "Husstandsmedlemmet bor i husstanden fra- og med måned")
+  val periodeFra: LocalDate,
+
+  @Schema(description = "Husstandsmedlemmet bor i husstanden til- og med måned")
+  val periodeTil: LocalDate?,
+
+  @Schema(description = "Manuelt opprettet av")
+  val opprettetAv: String?,
+
+  @Schema(description = "Lagret tidspunkt")
+  val lagretTidspunkt: LocalDateTime
+)
+
+data class EgneBarnDto(
+  @Schema(description = "Liste over alle barn en person har en forelder-relasjon til. I tillegg til periodisert liste med når et barn bor sammen med forelderen")
+  val egneBarnListe: List<Barn>?
+)
+
+data class Barn(
+  @Schema(description = "Identen til barnet")
+  var personId: String?,
+
+  @Schema(description = "Barnets fødselsdato")
+  var foedselsdato: LocalDate?,
+
+  @Schema(description = "Barnets fødselsår")
+  var foedselsaar: LocalDate?,
+
+  @Schema(description = "Navn på personen, format <Fornavn, mellomnavn, Etternavn")
+  var navn: String?,
+
+  @Schema(description = "Barnets eventuelle dødsdato")
+  var doedssdato: LocalDate?,
+
+  @Schema(description = "Manuelt opprettet av")
+  val opprettetAv: String?,
+
+  @Schema(description = "Lagret tidspunkt")
+  val lagretTidspunkt: LocalDateTime,
+
+  @Schema(description = "Perioder barnet bor i samme husstand som aktuell forelder")
+  val borISammeHusstandListe: List<BorISammeHusstand>?
+)
+
+data class BorISammeHusstand(
+  @Schema(description = "Barnet bor i husstanden fra- og med måned")
+  val periodeFra: LocalDate,
+
+  @Schema(description = "Barnet bor i husstanden til- og med måned")
+  val periodeTil: LocalDate?,
+
+  @Schema(description = "Manuelt opprettet av")
+  val opprettetAv: String?,
+
+  @Schema(description = "Lagret tidspunkt")
+  val lagretTidspunkt: LocalDateTime
+)
+
+data class Sivilstand(
+  @Schema(description = "Sivilstand gjelder fra- og med måned")
+  val periodeFra: LocalDate,
+
+  @Schema(description = "Sivilstand gjelder til- og med måned")
+  val periodeTil: LocalDate?,
+
+  @Schema(description = "Personens sivilstand")
+  val sivilstand: SivilstandKode,
+
+  @Schema(description = "Manuelt opprettet av")
+  val opprettetAv: String?,
+
+  @Schema(description = "Lagret tidspunkt")
+  val lagretTidspunkt: LocalDateTime
 )
