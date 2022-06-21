db9f0f551952e07fa0f038adb3fcae939595736a
rinnan17 <magnus.rinnan@nav.no>
2022-06-21 13:27:28 +0200

    Fjerner innsynHistoriskeInntekterDato

--- src/main/kotlin/no/nav/bidrag/behandling/felles/dto/grunnlag/request.kt
+++ src/main/kotlin/no/nav/bidrag/behandling/felles/dto/grunnlag/request.kt
@@ -40,15 +40,15 @@ data class GrunnlagRequestDto(
   val type: GrunnlagRequestType,
 
   @Schema(description = "Angir personId som grunnlag skal hentes for")
-  @field:Pattern(regexp = "^[0-9]{11}\$", message = "Ugyldig format. Må inneholde eksakt 11 siffer.")
+  @field:Pattern(
+    regexp = "^[0-9]{11}\$",
+    message = "Ugyldig format. Må inneholde eksakt 11 siffer."
+  )
   val personId: String,
 
   @Schema(description = "Første periode det skal hentes ut grunnlag for (på formatet YYYY-MM-DD)")
   val periodeFra: LocalDate,
 
   @Schema(description = "Grunnlag skal hentes TIL denne perioden, på formatet YYYY-MM-DD")
-  val periodeTil: LocalDate,
-
-  @Schema(description = "Hent historiske inntekter fra Inntektskomponenten på angitt dato, skal kun brukes til engangsinnhenting av historikk")
-  val innsynHistoriskeInntekterDato: LocalDate? = null
+  val periodeTil: LocalDate
 )
